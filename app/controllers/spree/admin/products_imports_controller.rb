d7c53e148479eaadf242e989ae19f4ef1fc1c1ee
Kamil Farys <kamil.maniek@gmail.com>
2019-05-19 23:28:28 +0200

    Add service tests

--- app/controllers/spree/admin/products_imports_controller.rb
+++ app/controllers/spree/admin/products_imports_controller.rb
@@ -2,6 +2,12 @@ class Spree::Admin::ProductsImportsController < Spree::Admin::BaseController
   def new; end
 
   def create
+    if incorrect_file_send
+      flash[:error] = "File not sended or type is inncorrect"
+
+      redirect_to action: :new and return
+    end
+
     file_path = "tmp/#{SecureRandom.urlsafe_base64}.csv"
     File.write(file_path, params[:csv_file].read)
 
@@ -9,5 +15,11 @@ class Spree::Admin::ProductsImportsController < Spree::Admin::BaseController
     flash[:success] = "Background import runned successfully"
 
     redirect_to admin_products_path
+  end
+
+  private
+
+  def incorrect_file_send
+    params[:csv_file].nil? || params[:csv_file].path.split(//).last(3).join != "csv"
   end
 end
---
+++ spec/services/csv_products_importer_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe CSVProductsImporter do
+  context "Imports products" do
+    before do
+      Spree::StockLocation.create(name: 'default')
+    end
+
+    it "add products when file data are correct" do
+      CSVProductsImporter.new(file_path: "spec/mock_files/sample.csv").call
+
+      expect(Spree::Product.count).to eq(3)
+    end
+
+    it "not add data when file data are incorrect" do
+      CSVProductsImporter.new(file_path: "spec/mock_files/empty.csv").call
+
+      expect(Spree::Product.count).to eq(0)
+    end
+  end
+end
